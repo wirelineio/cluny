64c077c06c8b38c6915bd4aa32c9b1a4d3cc3a52
David Boreham <david@wireline.io>
2020-09-03 07:43:20 -0600

    Hack static network list

--- app/src/components/common/TmSelectNetwork.vue
+++ app/src/components/common/TmSelectNetwork.vue
@@ -26,10 +26,18 @@ export default {
   computed: {
     ...mapGetters([`networks`]),
     mainNetworks() {
-      return this.networks.filter((network) => !network.testnet)
+      return [
+        { id: 'mainnet', chain_id: 'wns-mainnet', testnet: false, title: 'Wireline Mainnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'mainnet', default: true }
+        ]
     },
     testNetworks() {
-      return this.networks.filter((network) => network.testnet)
+      return [
+        { id: 'moon', chain_id: 'moon', testnet: true, title: 'Moon Devnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'moon', default: true },
+        { id: 'phobos', chain_id: 'phobos', testnet: true, title: 'Phobos Testnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'phobos', default: true },
+        { id: 'mars', chain_id: 'mars', testnet: true, title: 'Mars Testnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'mars', default: true },
+        { id: 'mercury', chain_id: 'mercury', testnet: true, title: 'Mercury Testnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'mercury', default: true },
+        { id: 'venus', chain_id: 'venus', testnet: true, title: 'Venus Testnet', icon: 'https://avatars3.githubusercontent.com/u/57182821?s=60&v=4', slug: 'venus', default: true }
+        ]
     },
   },
 }
